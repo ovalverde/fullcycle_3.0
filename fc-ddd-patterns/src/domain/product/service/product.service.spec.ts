193871cb20e337958462679418ac980dbfbd7a29
Orlando Valverde <orlando.valverde@gmail.com>
2022-03-26 21:03:52 -0300

    product enhacements

--- fc-ddd-patterns/src/domain/product/service/product.service.spec.ts
+++ fc-ddd-patterns/src/domain/product/service/product.service.spec.ts
@@ -5,11 +5,13 @@ describe("Product service unit tests", () => {
   it("should change the prices of all products", () => {
     const product1 = new Product("product1", "Product 1", 10);
     const product2 = new Product("product2", "Product 2", 20);
-    const products = [product1, product2];
+    const product3 = new Product("product3","Product 3",100);
+    const products = [product1, product2,product3];
 
     ProductService.increasePrice(products, 100);
 
     expect(product1.price).toBe(20);
     expect(product2.price).toBe(40);
+    expect(product3.price).toBe(200);
   });
 });
